23a3883553e708596849a23d2a9d0711dc638667
jithendra-gunuru <jithendra.gunuru2016@gmail.com>
2024-06-08 18:52:12 +0530

    created a hello world program to add it in this repository

---
+++ Hello_world.py
@@ -0,0 +1,14 @@
+#we will print hello world in python
+
+
+#hello_world helper function
+def hello_world():
+    return "Hello world!!!!"
+
+def main():
+    message=hello_world()
+    print(message)
+    
+
+if __name__ == "__main__":
+    main()
